6fb06b0d219920f269955bb899da767bd126fb7a
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-09 15:13:18 +0000

    Adding sessions

--- includes/shortlist-functions.php
+++ includes/shortlist-functions.php
@@ -38,4 +38,42 @@ if(!isset($_SESSION['shortlist'])) {
 }
 
 
+// define variable defaults
+$action = null;
+$id = 0;
+ 
+// assign action and id parameters if set
+if ( isset( $_GET['action'] ) && !empty( $_GET['action'] ) ) {
+    //the action from the URL
+    $action = $_GET['action'];
+}
+if ( isset( $_GET['id'] ) && !empty( $_GET['id'] ) ) {
+    //the item id from the URL
+    $id = $_GET['id'];
+}
+
+
+switch($action) {  
+ 
+    case "add":
+        // check if item is already in array, if not, add
+        if(($key = array_search($id, $_SESSION['shortlist'])) === false) {
+            array_push( $_SESSION['shortlist'], $id );
+        }
+    break;
+     
+    case "remove":
+        // search for item by value and remove if found
+        if(($key = array_search($id, $_SESSION['shortlist'])) !== false) {
+            unset($_SESSION['shortlist'][$key]);
+        }
+    break;
+     
+    case "empty":
+        //remove all
+        unset($_SESSION['shortlist']);
+    break;
+ 
+}
+
 ?>
---
+++ includes/shortlist-total.php
@@ -0,0 +1,15 @@
+<?php
+
+
+//start the session
+if(!session_id()) {
+    session_start();
+}
+ 
+if(isset($_SESSION['shortlist'])) {
+    echo count($_SESSION['shortlist']);
+} else {
+    echo '0';
+}  
+
+?>
